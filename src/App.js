2c8a57e8e9ae5829b5b4d367e23169d194bc535e
MS04566 <MS04566@kbcard-icloud.com>
2020-03-03 16:14:17 +0900

    commit test

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React Test
+            Learn React Test11
           </a>
         </header>
       </div>
